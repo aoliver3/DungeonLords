8d0c1ed0506b70bb2a1e34905f938c77409161fc
Anthony Oliver <aoliver3@ggc.edu>
2015-03-23 15:59:36 -0400

    this is a test to see if the uploads work through eclipse and GitHub

---
+++ DungeonLords/src/Tester.java
@@ -0,0 +1,16 @@
+/**Class: Tester.java
+ * @author Anthony Oliver
+ * @version 1.0 <p>
+ * Course : ITEC 3150 Spring 2015
+ * Written: Mar 23, 2015
+ *
+ *
+ * This class – now describe what the class does
+ *
+ * Purpose: – Describe the purpose of this class
+ */
+
+public class Tester
+{
+	//this is a test to see if the upload works and creates a branch in GitHub
+}
