bb4994c81b03385c5e266f267b871e030339c811
Anthony Oliver <aoliver3@ggc.edu>
2015-04-12 23:52:33 -0400

    uploading tester classes for Game, Dungeon & Player

---
+++ DungeonLords/src/GameTest.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+
+/**Class: GameTest.java
+ * @author Anthony Oliver
+ * @version 1.0 <p>
+ * Course : ITEC 3150 Spring 2015
+ * Written: Apr 12, 2015
+ *
+ *
+ * This class – now describe what the class does
+ *
+ * Purpose: – Describe the purpose of this class
+ */
+
+public class GameTest
+{
+
+	/**
+	 * this will be a method to test my game class and its methods
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		//////////////////////////////////
+		// create a dungeon for my game //
+		//////////////////////////////////
+
+		//create a new player
+		Player p = new Player("Anthony");
+		//create an array list of room exits
+		ArrayList<Room> exit = new ArrayList<Room>();
+		//create rooms for the dungeon
+		Room r1 = new Room("room 1", "the ok room", exit, true);
+		Room r2 = new Room("room 2", "the good room", exit, false);
+		Room r3 = new Room("room 3", "the great room", exit, true);
+		//put the rooms in an array list
+		ArrayList<Room> al = new ArrayList<Room>();
+		al.add(r1); al.add(r2); al.add(r3);
+		//add the player and the rooms to the dungeon
+		Dungeon d = new Dungeon(p, al);
+
+		/////////////////////////////
+		// put my dungeon together //
+		/////////////////////////////
+
+		Game g = new Game(d);
+		System.out.println(g.getGameDungeon().getUser().getPlayerName()); //expected output Anthony
+
+		//////////////////////////////////////////////
+		// create another dungeon to alter the game //
+		//////////////////////////////////////////////
+
+		//create a new player
+		Player p2 = new Player("Joey");
+		//create an array list of room exits
+		ArrayList<Room> exit2 = new ArrayList<Room>();
+		//create rooms for the dungeon
+		Room r4 = new Room("room 4", "the awesome room", exit2, true);
+		Room r5 = new Room("room 5", "the gnarly room", exit2, true);
+		Room r6 = new Room("room 6", "the tubular room", exit2, true);
+		//put the rooms in an array list
+		ArrayList<Room> al2 = new ArrayList<Room>();
+		al2.add(r4); al2.add(r5); al2.add(r6);
+		//setting the dungeon to the new player and room variables
+		Dungeon d2 = new Dungeon(p2, al2);
+		
+		//altering the game dungeon
+		g.setGameDungeon(d2);
+		
+		//testing to see if the changes took effect
+		System.out.println(g.getGameDungeon().getUser().getPlayerName()); //expected output Joey
+		
+	}
+
+}
---
+++ DungeonLords/src/PlayerTest.java
@@ -0,0 +1,47 @@
+/**Class: PlayerTest.java
+ * @author Anthony Oliver
+ * @version 1.0 <p>
+ * Course : ITEC 3150 Spring 2015
+ * Written: Apr 12, 2015
+ *
+ *
+ * This class – now describe what the class does
+ *
+ * Purpose: – Describe the purpose of this class
+ */
+
+public class PlayerTest
+{
+
+	/**
+	 * this method test my player class and its methods
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		//create a new player to test ethods
+		Player p = new Player("Anthony");
+		System.out.println(p.getPlayerName());  //expected Anthony
+		System.out.println(p.getCurrentHealth());  //expected 150
+		System.out.println(p.getMaxHealth());  //expected 150
+		System.out.println(p.getPlayaerDamage());  //expected 0
+		System.out.println(p.getPlayerMana());// expected 100
+		
+		//changing players attributes
+		p.setMaxHealth(200);
+		p.setCurrentHealth(175);
+		p.setPlayerName("Joey");
+		p.setPlayaerDamage(25);
+		p.setPlayerMana(50);
+		
+		System.out.println("");
+		//reprinting to make sure the change took effect
+		System.out.println(p.getPlayerName());  //expected Joey
+		System.out.println(p.getCurrentHealth());  //expected 175
+		System.out.println(p.getMaxHealth());  //expected 200
+		System.out.println(p.getPlayaerDamage());  //expected 25
+		System.out.println(p.getPlayerMana());  //expected 50
+
+	}
+
+}
